62a87f4dfa9a0c5d56ad6f3dbcefe92e9583c0e0
LironHazan <liron@gigaspaces.com>
2014-12-25 17:30:57 +0200

    changing message

--- butterfly/routes.py
+++ butterfly/routes.py
@@ -51,6 +51,7 @@ def u(s):
 def motd(socket):
     return (
 '''
+B
 
  .o88b. db       .d88b.  db    db d8888b. d888888b d88888b db    db
 d8P  Y8 88      .8P  Y8. 88    88 88   8D    88    88       8b  d8
@@ -59,7 +60,7 @@ d8P  Y8 88      .8P  Y8. 88    88 88   8D    88    88       8b  d8
 Y8b  d8 88booo.  8b  d8' 88b  d88 88  .8D   .88.   88         88
   Y88P  Y88888P   Y88P'  ~Y8888P' Y8888D' Y888888P YP         YP
 
-
+X
 '''
         .replace('B', '\x1b[34;1m')
         .replace('G', '\x1b[32;1m')
